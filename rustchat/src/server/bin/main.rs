9b42adc0995ec1466709c3909ddb54ab25d11392
applecare1234 <raspy1108@gmail.com>
2021-06-09 02:17:13 -0700

    echoi

--- rustchat/src/server/bin/main.rs
+++ rustchat/src/server/bin/main.rs
@@ -1,10 +1,27 @@
+use std::io::{BufRead, BufReader, BufWriter};
+use std::io::Write;
 use std::process::exit;
 use std::net::{Ipv4Addr, SocketAddr, TcpListener, TcpStream};
+use std::thread::spawn;
 
-use librustchat::user::User;
+//각 클라이언트의 데이터를 받아 처리하는 스레드.
+fn message_handler() {
 
-fn handler(stream: TcpStream) {
-
+}
+fn client_handler(stream: TcpStream) {
+    let addr = stream.peer_addr().unwrap();
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
+    let mut writer = BufWriter::new(stream.try_clone().unwrap());
+    println!("addr: {}", addr);
+    loop {
+        let mut s = String::new();
+        reader.read_line(&mut s).unwrap();
+        if s.eq("end\n") {
+            break;
+        }
+        writer.write(s.as_bytes()).unwrap();
+        writer.flush().unwrap();
+    }
 }
 fn main(){
     let arguments : Vec<String> = std::env::args().collect();
@@ -21,12 +38,14 @@ fn main(){
         .expect("not a port number!");
 
     let listener = TcpListener::bind(SocketAddr::from((ip, port))).unwrap();
-    let mut userlist: Vec<User> = Vec::new();
+
+    println!("listening: {}:{}")
+    //let mut userlist: Vec<User> = Vec::new();
     
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let addr = stream.peer_addr().unwrap();
-        println!("connected: {}", addr);
-        handler(stream);
+        spawn(|| {
+            client_handler(stream);
+        });
     }
 }
